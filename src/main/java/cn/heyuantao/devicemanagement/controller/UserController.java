19a083a0c7897e32e30c5a51b463623f2e01c906
heyuantao <he_yuan_tao@163.com>
2020-06-20 17:06:38 +0800

    new class

--- src/main/java/cn/heyuantao/devicemanagement/controller/UserController.java
+++ src/main/java/cn/heyuantao/devicemanagement/controller/UserController.java
@@ -1,9 +1,13 @@
 package cn.heyuantao.devicemanagement.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-
     //public String
+    @RequestMapping("/users")
+    public String hello(){
+        return "hello world !";
+    }
 }
