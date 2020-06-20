a986bc1e5ca542d01178962823f85d641290da24
heyuantao <he_yuan_tao@163.com>
2020-06-20 21:36:31 +0800

    new class

---
+++ src/main/java/cn/heyuantao/devicemanagement/controller/TestController.java
@@ -0,0 +1,26 @@
+package cn.heyuantao.devicemanagement.controller;
+
+import cn.heyuantao.devicemanagement.domain.User;
+import cn.heyuantao.devicemanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public ModelAndView list(Model model)
+    {
+        //model.addAttribute("userList", userRepository.listUsers());
+        //model.addAttribute("title","用户管理");
+        return new ModelAndView("test/index");
+    }
+
+}
