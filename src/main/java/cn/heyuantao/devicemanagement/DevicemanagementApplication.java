39794b5520c6595f99bbc83714e6f32dc1944de0
heyuantao <he_yuan_tao@163.com>
2020-06-20 10:11:39 +0800

    first commit

---
+++ src/main/java/cn/heyuantao/devicemanagement/DevicemanagementApplication.java
@@ -0,0 +1,13 @@
+package cn.heyuantao.devicemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicemanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevicemanagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/heyuantao/devicemanagement/DevicemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.heyuantao.devicemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicemanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
