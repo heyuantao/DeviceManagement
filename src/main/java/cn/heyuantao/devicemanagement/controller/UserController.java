e1d882d154beec56ac4442afe03bfde9da7e0787
heyuantao <he_yuan_tao@163.com>
2020-06-20 19:05:30 +0800

    new class

--- src/main/java/cn/heyuantao/devicemanagement/controller/UserController.java
+++ src/main/java/cn/heyuantao/devicemanagement/controller/UserController.java
@@ -1,9 +1,11 @@
 package cn.heyuantao.devicemanagement.controller;
 
+import cn.heyuantao.devicemanagement.domain.User;
 import cn.heyuantao.devicemanagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,5 +24,21 @@ public class UserController {
         model.addAttribute("userList", userRepository.listUsers());
         model.addAttribute("title","用户管理");
         return new ModelAndView("users/list","userModel",model);
+    }
+
+    @GetMapping("{id}")
+    public ModelAndView view(@PathVariable("id") Long id, Model model){
+        User user = userRepository.getUserById(id);
+        model.addAttribute("user",user);
+        model.addAttribute("title","查看用户");
+        return new ModelAndView("users/view","userModel",model);
+    }
+
+    @GetMapping("/form")
+    public ModelAndView createForm(Model model){
+        User user = new User();
+        model.addAttribute("user",user);
+        model.addAttribute("title","创建用户");
+        return new ModelAndView("users/form","userModel",model);
     }
 }
