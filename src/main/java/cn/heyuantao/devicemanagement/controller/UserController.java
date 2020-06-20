1487437f005b3ec3a93b16549e21621e2862b63e
heyuantao <he_yuan_tao@163.com>
2020-06-20 18:38:25 +0800

    new class

--- src/main/java/cn/heyuantao/devicemanagement/controller/UserController.java
+++ src/main/java/cn/heyuantao/devicemanagement/controller/UserController.java
@@ -1,13 +1,26 @@
 package cn.heyuantao.devicemanagement.controller;
 
+import cn.heyuantao.devicemanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
-    //public String
-    @RequestMapping("/users")
-    public String hello(){
-        return "hello world !";
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public ModelAndView list(Model model)
+    {
+        model.addAttribute("userList", userRepository.listUsers());
+        model.addAttribute("title","用户管理");
+        return new ModelAndView("users/list","userModel",model);
     }
 }
