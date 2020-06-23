1097183779cdf0b6c97cbdfe5373ba2ef42d75d0
heyuantao <he_yuan_tao@163.com>
2020-06-23 19:33:32 +0800

    new class

--- src/main/java/cn/heyuantao/devicemanagement/config/ShiroConfig.java
+++ src/main/java/cn/heyuantao/devicemanagement/config/ShiroConfig.java
@@ -1,5 +1,6 @@
 package cn.heyuantao.devicemanagement.config;
 
+import org.apache.shiro.realm.text.IniRealm;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,16 @@ import org.springframework.context.annotation.Configuration;
 public class ShiroConfig {
 
     @Bean
-    public
+    public IniRealm getIniReam(){
+        return new IniRealm("classpath:shiro.ini");
+    }
+
+    @Bean
+    public DefaultWebSecurityManager getDefaultWebSecurityManager(IniRealm iniRealm){
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(iniRealm);
+        return securityManager;
+    }
 
     @Bean
     public ShiroFilterFactoryBean shiroFilter(DefaultWebSecurityManager securityManager){
