c1f4c61dba5328651db3a2a835ff33397373aff5
heyuantao <he_yuan_tao@163.com>
2020-07-10 19:25:21 +0800

    login success

--- db_init.sql
+++ db_init.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE `devicemanagement`;
+
 USE`devicemanagement`;
 
 CREATE TABLE `user` (
@@ -6,8 +7,18 @@ CREATE TABLE `user` (
     `username` CHAR(30) NOT NULL,
     `password` CHAR(30) NOT NULL,
     `email` CHAR(30) ,
-) 
+    `superuser` TINYINT NOT NULL DEFAULT 0
+);
 
-create table `device` (
-    `id` integer not null ,
+CREATE TABLE `location` (
+    `id` INT(20) NOT NULL PRIMARY KEY,
+    `name` CHAR(30) NOT NULL,
+    `describe` TEXT
+);
+
+CREATE TABLE `device` (
+    `id`            INT(20) NOT NULL PRIMARY KEY ,
+    `name`          CHAR(30) NOT NULL ,
+    `in_date`       DATE NOT NULL ,
+    `location_id`   INT(20)
 )
