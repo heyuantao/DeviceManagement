9f00d727e7a4a1e028e382e6d9f6ebf7695354a8
heyuantao <he_yuan_tao@163.com>
2020-07-10 21:39:37 +0800

    login success

--- db_init.sql
+++ db_init.sql
@@ -1,24 +1,54 @@
-CREATE DATABASE `devicemanagement`;
+CREATE DATABASE devicemanagement;
 
-USE`devicemanagement`;
+USE devicemanagement;
 
+#系统用户表
 CREATE TABLE `user` (
-    `id` INT(20) NOT NULL PRIMARY KEY,
-    `username` CHAR(30) NOT NULL,
-    `password` CHAR(30) NOT NULL,
-    `email` CHAR(30) ,
-    `superuser` TINYINT NOT NULL DEFAULT 0
-);
+    `id`          INT PRIMARY KEY AUTO_INCREMENT COMMENT '用户ID',
+    `name`       CHAR(30) NOT NULL COMMENT '用户姓名',
+    `password`    CHAR(30) NOT NULL COMMENT '用户密码',
+    `email`       CHAR(30)  COMMENT '用户邮箱',
+    `superuser`   TINYINT NOT NULL DEFAULT 0 COMMENT '是否为管理员'
+)ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
+#位置信息表,设置为设备存放的位置
 CREATE TABLE `location` (
-    `id` INT(20) NOT NULL PRIMARY KEY,
-    `name` CHAR(30) NOT NULL,
-    `describe` TEXT
-);
+    `id`          INT PRIMARY KEY AUTO_INCREMENT COMMENT '位置ID',
+    `name`        CHAR(30) NOT NULL COMMENT '位置名称',
+    `describe`    TEXT COMMENT '位置描述'
+)ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
-CREATE TABLE `device` (
-    `id`            INT(20) NOT NULL PRIMARY KEY ,
-    `name`          CHAR(30) NOT NULL ,
-    `in_date`       DATE NOT NULL ,
-    `location_id`   INT(20)
-)
+#设备的类型
+CREATE TABLE `type` (
+    `id`          INT PRIMARY KEY AUTO_INCREMENT COMMENT '类型ID',
+    `name`        CHAR(30) NOT NULL COMMENT '设备类型名称',
+    `describe`    TEXT COMMENT '设备类型描述'
+)ENGINE=INNODB DEFAULT CHARSET=UTF8;
+
+#设备保管人
+CREATE TABLE `owner` (
+    `id`          INT PRIMARY KEY AUTO_INCREMENT COMMENT '保管人ID',
+    `name`       CHAR(30) NOT NULL COMMENT '保管人名称',
+    `describe`    TEXT COMMENT '保管人描述'
+)ENGINE=INNODB DEFAULT CHARSET=UTF8;
+
+#具体的设备
+CREATE TABLE device (
+    `id`            INT PRIMARY KEY AUTO_INCREMENT COMMENT '设备ID',
+    `name`          CHAR(30) NOT NULL COMMENT '设备名称',
+    `sn`            CHAR(30) NOT NULL COMMENT '设备序列号',
+    `asset_no`      CHAR(30) NOT NULL COMMENT '固定资产编号',
+    `in_date`       DATE NOT NULL COMMENT '设备购买时间',
+    `location_id`   INT COMMENT '设备的位置外键ID',
+    `type_id`       INT COMMENT '设备的类型外键ID',
+    `owner_id`      INT COMMENT '设备的保管人外键ID',
+    `updated`       DATE NOT NULL COMMENT '记录更新时间',
+    FOREIGN KEY (location_id)   REFERENCES location(id),
+    FOREIGN KEY (type_id)       REFERENCES type(id),
+    FOREIGN KEY (owner_id)      REFERENCES owner(id)
+    #CONSTRAINT location_fk  FOREIGN KEY (location_id)   REFERENCES location(id),
+    #CONSTRAINT type_fk      FOREIGN KEY (type_id)       REFERENCES type(id),
+    #CONSTRAINT owner_fk     FOREIGN KEY (owner_id)       REFERENCES owner(id)
+    #ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=INNODB DEFAULT CHARSET=UTF8;
+
