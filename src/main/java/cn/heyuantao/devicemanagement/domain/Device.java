a943d51989d015f2411f4c59524d01c66ea580a5
heyuantao <he_yuan_tao@163.com>
2020-07-10 21:43:43 +0800

    login success

--- src/main/java/cn/heyuantao/devicemanagement/domain/Device.java
+++ src/main/java/cn/heyuantao/devicemanagement/domain/Device.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
 import java.util.Date;
 
 
@@ -15,8 +12,6 @@ import java.util.Date;
 @NoArgsConstructor
 @Data
 public class Device {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String vender;
