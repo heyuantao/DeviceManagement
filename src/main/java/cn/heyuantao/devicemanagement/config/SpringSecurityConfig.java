a42c08d6c3fbc7e510b2218aa3cbaa5fff55e56b
heyuantao <he_yuan_tao@163.com>
2020-07-15 23:14:30 +0800

    have a rest

--- src/main/java/cn/heyuantao/devicemanagement/config/SpringSecurityConfig.java
+++ src/main/java/cn/heyuantao/devicemanagement/config/SpringSecurityConfig.java
@@ -32,7 +32,6 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //super.configure(http);
         http
                 .csrf().disable()
                 .authorizeRequests()
