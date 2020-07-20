9b9ad4bf6433bf045c78817d4ecfacbbd219af93
heyuantao <he_yuan_tao@163.com>
2020-07-20 23:04:43 +0800

    login and logout program work

--- src/main/java/cn/heyuantao/devicemanagement/controller/IndexPage.java
+++ src/main/java/cn/heyuantao/devicemanagement/controller/IndexPage.java
@@ -5,21 +5,21 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author he_yu
  * 首页信息
  */
-@RestController
+@Controller
 @RequestMapping("/")
-public class IndexController {
+public class IndexPage {
 
-
+    @GetMapping
     public String home(Model model){
-        //AnonymousAuthenticationToken
         String usernameForDisplay = "";
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
--- src/main/java/cn/heyuantao/devicemanagement/controller/LoginAndRegisterController.java
+++ src/main/java/cn/heyuantao/devicemanagement/controller/LoginAndRegisterController.java
@@ -3,6 +3,8 @@ package cn.heyuantao.devicemanagement.controller;
 import cn.heyuantao.devicemanagement.auth.CustomUserDetails;
 import cn.heyuantao.devicemanagement.dto.APILoginRequestDTO;
 import cn.heyuantao.devicemanagement.exception.ErrorDetails;
+import cn.heyuantao.devicemanagement.exception.ValidateCaptchaException;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,15 +13,22 @@ import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -33,17 +42,33 @@ public class LoginAndRegisterController {
     AuthenticationManager authenticationManager;
 
 
+
     //负责对接用ajax方式提交的api的login请求
     @PostMapping("/api/v1/login")
     public ResponseEntity<?> loginAPI(@RequestBody APILoginRequestDTO apiLoginRequestDTO, HttpServletRequest httpServletRequest){
-        System.out.println("This is api post information !");
-        System.out.println(apiLoginRequestDTO.getUsername());
-        System.out.println(apiLoginRequestDTO.getPassword());
-        System.out.println(apiLoginRequestDTO.getCaptcha());
+
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken= new UsernamePasswordAuthenticationToken(
                 apiLoginRequestDTO.getUsername(),
                 apiLoginRequestDTO.getPassword()
         );
+
+        HttpSession httpSession= httpServletRequest.getSession();
+        String captchaInSession = (String) httpSession.getAttribute("captcha");
+        String captchaInRequest = apiLoginRequestDTO.getCaptcha();
+
+        if(StringUtils.isBlank(captchaInRequest)){
+            ErrorDetails errorDetails=new ErrorDetails("验证码为空","");
+            return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+        }
+        if(captchaInSession==null){
+            ErrorDetails errorDetails=new ErrorDetails("验证码为空","");
+            return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+        }
+        if(!StringUtils.equalsIgnoreCase(captchaInRequest,captchaInSession)){
+            ErrorDetails errorDetails=new ErrorDetails("验证码错误","");
+            return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+        }
+
         Authentication authentication;
         try{
             authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
@@ -51,9 +76,28 @@ public class LoginAndRegisterController {
             ErrorDetails errorDetails=new ErrorDetails("登录失败",ex.getMessage());
             return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
         }
-        //usernamePasswordAuthenticationToken.setDetails(authenticationDetailsSource.buildDetails(httpServletRequest));
         usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return ResponseEntity.ok("success");
+
+        return new ResponseEntity(new HashMap<>(),HttpStatus.ACCEPTED);
     }
+
+    @PostMapping("/api/v1/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response,
+                                    Authentication authentication){
+/*        Assert.notNull(request, "HttpServletRequest required");
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            //logger.debug("Invalidating session: " + session.getId());
+            session.invalidate();
+        }
+
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(null);
+        SecurityContextHolder.clearContext();*/
+        SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+        securityContextLogoutHandler.logout(request, response, authentication);
+        return new ResponseEntity(new HashMap<>(),HttpStatus.ACCEPTED);
+    };
 }
