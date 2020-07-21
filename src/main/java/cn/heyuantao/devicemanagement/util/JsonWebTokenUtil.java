768280bd9515cf7cc7a29afab330a121e07f558f
heyuantao <he_yuan_tao@163.com>
2020-07-21 16:55:53 +0800

    have a rest

--- src/main/java/cn/heyuantao/devicemanagement/util/JsonWebTokenUtil.java
+++ src/main/java/cn/heyuantao/devicemanagement/util/JsonWebTokenUtil.java
@@ -79,7 +79,7 @@ public class JsonWebTokenUtil {
     private String createToken(Map<String,Object> claims, String subject){
 
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*60*10))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*60*1))
                 .signWith(SignatureAlgorithm.HS256,secret).compact();
     }
 
