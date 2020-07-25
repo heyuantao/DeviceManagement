988892dec7c3abfd965bbacefd6e42664812e8fb
heyuantao <he_yuan_tao@163.com>
2020-07-25 16:17:42 +0800

    use jpa

--- src/main/java/cn/heyuantao/devicemanagement/service/LocationService.java
+++ src/main/java/cn/heyuantao/devicemanagement/service/LocationService.java
@@ -9,27 +9,25 @@ import java.util.List;
  * @author he_yu
  */
 public interface LocationService {
-    /**
+    /**获取所有的设备保存地址
      * 获得所有位置
      * @return
      */
     List<Location> getLocations();
 
-    /**
-     *
+    /**添加一个设备存放地址
      * @param oneLocation
      * @return
      */
     Location addLocation(Location oneLocation);
 
-    /**
-     *
+    /**通过ID来获取设备保存地址
      * @param id
      * @return
      */
     Location getLocationById(Long id);
 
-    /**
+    /**更新设备保存地址
      *
      * @param id 位置在数据库的编号
      * @param location  位置对象
@@ -37,8 +35,7 @@ public interface LocationService {
      */
     Location updateLocationById(Long id, Location location);
 
-    /**
-     * 删除指定的存放位置
+    /**删除指定的存放位置
      * @param id
      */
     void deleteLocationById(Long id);
