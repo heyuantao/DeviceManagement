bac96c7a68b79e762676d7860e6bbcd0c0f506c7
heyuantao <he_yuan_tao@163.com>
2020-08-09 23:45:13 +0800

    preauthorize anno work fine

--- src/main/java/cn/heyuantao/devicemanagement/controller/LocationController.java
+++ src/main/java/cn/heyuantao/devicemanagement/controller/LocationController.java
@@ -10,6 +10,7 @@ import com.github.pagehelper.PageHelper;
 import io.swagger.annotations.Api;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
---
+++ src/main/java/cn/heyuantao/devicemanagement/service/PermissionService.java
@@ -0,0 +1,57 @@
+package cn.heyuantao.devicemanagement.service;
+
+import cn.heyuantao.devicemanagement.auth.CustomUserDetails;
+import cn.heyuantao.devicemanagement.domain.User;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+/**
+ * 该服务定义了基本的权限框架，配合在接口上使用@PreAuthorize注解来完成权限的检查，样例如下
+ * @PreAuthorize("@ss.testPermission()")
+ * 该模块未完全实现，请勿使用
+ * @author he_yu
+ */
+@Service("ss")
+public class PermissionService {
+
+
+    /**
+     * 使用 Spring Security 获取当前的用户信息
+     * @return
+     */
+    private User getCurrentUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+        User user = customUserDetails.getUser();
+        return user;
+    }
+
+
+    /**
+     * 使用用户的名字来检查相应的权限
+     * @param permission
+     * @return
+     */
+    public Boolean hasPermission(String permission){
+        User user = getCurrentUser();
+        System.out.println("Check permission in preauthorize !");
+        if(user==null) {
+            return Boolean.FALSE;
+        }else if(StringUtils.equalsIgnoreCase(user.getName(),"admin")){
+            return Boolean.TRUE;
+        }else{
+            return Boolean.FALSE;
+        }
+    }
+
+    /**
+     * 仅仅是测试这种方法是否能用，配合@PreAuthorize放在控制器上来使用，例子如下
+     * @PreAuthorize("@ss.testPermission()")
+     * @return
+     */
+    public Boolean testPermission(){
+        return Boolean.FALSE;
+    }
+}
